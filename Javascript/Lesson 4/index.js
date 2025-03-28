041c3f138e2041d84d830ee3b076fdc93c008d9b
Michael Schubert <mschubert112@gmail.com>
2025-03-28 14:55:48 -0700

    Create index.js

---
+++ Javascript/Lesson 4/index.js
@@ -0,0 +1,5 @@
+// getting to know comments. This will literally just be comments.
+//single comments follow to forward slashes.
+
+/* a multi line comment is like CSS comments
+forward slash and star, then finish it with star, then forward slash*/
