377b19f8df1eb03ccc95a7be9d213e82d28dd4b8
Michael Schubert <mschubert112@gmail.com>
2025-03-28 14:37:41 -0700

    Create index.js

---
+++ Javascript/Lesson 2/index.js
@@ -0,0 +1,5 @@
+//console.log is used heavily, my guess is it's used a lot for debugging. We'll see what else
+//quotes need to match, so choose single or double quotes and stick with it.
+//this lesson is more familiarity with console.log
+
+console.log('my favorite food is teriyaki');
