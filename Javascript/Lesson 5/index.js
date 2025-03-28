882b7b4a5128ec6b3dc5414458bc848ccb997ff2
Michael Schubert <mschubert112@gmail.com>
2025-03-28 15:03:08 -0700

    Create index.js

---
+++ Javascript/Lesson 5/index.js
@@ -0,0 +1,12 @@
+//more console.log practice. This is an exercise to create a receipt with only console.log
+console.log("============================");
+console.log("        Taco  receipt       ");
+console.log("----------------------------");
+console.log('        Order Details       ');
+console.log("----------------------------");
+console.log("Item      quantity     price");
+console.log("----------------------------");
+console.log("Pollo Taco   2         3.99 ");
+console.log("----------------------------");
+console.log("Total                  3.99 ");
+console.log("============================");
