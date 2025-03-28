102b5d3171cf09b6f321ba0e452b3a00d8c25404
Michael Schubert <mschubert112@gmail.com>
2025-03-28 14:43:53 -0700

    Update index.js

--- Javascript/Lesson 3/index.js
+++ Javascript/Lesson 3/index.js
@@ -1,1 +1,8 @@
+//console.log exercise, making a letter tree using only console.log
+//pretty silly, but concept understood. console.log let's you print messages, like python print function
 
+console.log('   a');
+console.log('  b c');
+console.log(' d e f');
+console.log('g h i j');
+console.log('   k');
