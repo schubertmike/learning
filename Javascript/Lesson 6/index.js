a109fa49302cc03a0d27fb762467dcd9e6697c61
Michael Schubert <mschubert112@gmail.com>
2025-03-31 12:47:33 -0700

    Update index.js

--- Javascript/Lesson 6/index.js
+++ Javascript/Lesson 6/index.js
@@ -1,3 +1,6 @@
+/*learning about variable types. We have const (for constant variables that won't change), and let (for variables that will change). As you can see below, we've reassigned
+one of the variables */
+
 const firstName = 'Mike'
 const favoriteColor = 'Green'
 let currentLocation = 'Yum Town'
