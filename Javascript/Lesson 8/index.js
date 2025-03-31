463dfeea83f67713ea5748359cf53107e88f72b4
Michael Schubert <mschubert112@gmail.com>
2025-03-31 12:58:58 -0700

    Create index.js

---
+++ Javascript/Lesson 8/index.js
@@ -0,0 +1,7 @@
+// now learning about our arithmetic operator syntax in javascript
+//we have addition +, subtraction -, multiplication *, division /, modulo % (returns the remainder), and exponent **
+//below is a simple exercise to convert the current temperature from Fahrenheit to Celsius
+let currentTempF = 50; //current tempurature at my location (hardcoded, but changeable, using a let variable), in F degrees
+let newTempC = (currentTempF - 32) / 1.8; //conversion from F degrees to C degrees using the conversion formula of C temp = (F degrees - 32) / 1.8
+
+console.log(newTempC); //print C degrees temp, based on the above conversion
