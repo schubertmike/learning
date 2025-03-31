9d4a418255249a642635644f634c9b46b443960b
Michael Schubert <mschubert112@gmail.com>
2025-03-31 12:35:49 -0700

    Create index.js

---
+++ Javascript/Lesson 6/index.js
@@ -0,0 +1,10 @@
+const firstName = 'Mike'
+const favoriteColor = 'Green'
+let currentLocation = 'Yum Town'
+let currentMood = 'Happy'
+currentMood = 'Hungies'
+
+console.log('First Name - ' + firstName)
+console.log('Favorite Color - ' + favoriteColor)
+console.log('Current Location - ' + currentLocation)
+console.log('Current Mood - ' + currentMood)
