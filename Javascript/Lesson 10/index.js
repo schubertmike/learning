dec8899a133c2cb63ee40baa895ed90b863f1367
Michael Schubert <mschubert112@gmail.com>
2025-03-31 13:23:22 -0700

    Create index.js

---
+++ Javascript/Lesson 10/index.js
@@ -0,0 +1,5 @@
+//simple conversion exercise, just like the previous lesson.
+let earthWeight = 240.9; // current weight in lbs on earth
+let marsWeight = earthWeight * 0.38; // formula to get weight on mars, based on earth weight
+
+console.log(marsWeight); // print mars weight
