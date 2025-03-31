a1b330439f1c491161828a8a7a193ff6bfdd8a6d
Michael Schubert <mschubert112@gmail.com>
2025-03-31 12:49:58 -0700

    Create index.js

---
+++ Javascript/Lesson 7/index.js
@@ -0,0 +1,10 @@
+// learning about data types, starting with the basics (string, number, boolean, undefined)
+let companyName = 'Blue Goose'; //this is a string
+const foundingYear = 2025; //this is a number, I used a const variable because this value won't change
+let isActive = true; //this is a boolean value, it can be true or false.
+let fundingAmount; //this is undefined
+
+console.log(companyName); //print string variable
+console.log(foundingYear); //print number variable
+console.log(isActive); //print boolean variable
+console.log(fundingAmount); //print undefined variable
