c59494becc5ac1f04edca939aa7ae696243c999a
Michael Schubert <mschubert112@gmail.com>
2025-04-01 16:29:15 -0700

    Create index.js

---
+++ Javascript/Lesson 20/index.js
@@ -0,0 +1,4 @@
+//getting familiar with for loops, initial value, end value, and incrementation
+for (let i = 0; i <= 100; i++) { //for loop, i starting at 0, iterate until less than or equal to 100, iterate in increments of 1
+  console.log("I Must Not Tell Lies"); //each iteration, print the console log
+}
