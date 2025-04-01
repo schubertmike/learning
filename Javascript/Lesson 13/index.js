4878d90dabd404b5d5b014b33611fd120c6b61bb
Michael Schubert <mschubert112@gmail.com>
2025-04-01 14:12:58 -0700

    Create index.js

---
+++ Javascript/Lesson 13/index.js
@@ -0,0 +1,11 @@
+// learning about else
+
+let hour = 14; //hardcodes hour at 2pm using 24 hour cycle
+
+if(hour < 12) {
+  console.log('Good Morning'); //print good morning
+  console.log('Time to Brush your teeth, take a shower, then go workout before you start work'); //print a few things to do this morning
+} else {
+  console.log('Good Afternoon'); //for any other number, print good afternoon
+  console.log('finish up work, work on your code, and chill, the afternoon is yours'); //adding afternoon reminders
+}
