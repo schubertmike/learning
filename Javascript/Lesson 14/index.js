4077b6c21d457110375178a57f467651ca8addc7
Michael Schubert <mschubert112@gmail.com>
2025-04-01 14:18:24 -0700

    Create index.js

---
+++ Javascript/Lesson 14/index.js
@@ -0,0 +1,10 @@
+//learning about the else if statement, and paring it with if and else
+let pH = 7
+
+if (pH > 7) { //checking for basic ph balance
+  console.log('The PH balance is Basic');
+} else if (pH < 7) { // checking for acidic ph balance
+  console.log('The PH balance is Acidic');
+} else { //if neither, then output neutral ph balance
+  console.log('The PH balance is Neutral');
+}
