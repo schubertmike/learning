ccae1fa3cc4f57941d80adbfde0c77d2550b967e
Michael Schubert <mschubert112@gmail.com>
2025-04-01 14:04:19 -0700

    Create index.js

---
+++ Javascript/Lesson 11/index.js
@@ -0,0 +1,8 @@
+//learning about conditionals
+let num = Math.random(); //variable num = random number generator. my guess is between 0 and 1
+
+if(num > 0.5) { //conditional if the random number is greater than .5
+  console.log("Heads"); // print heads
+} else { // else its any other number (.5 and lower)
+  console.log("Tails"); // print tails
+}
