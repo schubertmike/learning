89f79e5c81e215ae575816706fb60312881995e0
Michael Schubert <mschubert112@gmail.com>
2025-04-02 14:08:17 -0700

    Create index.js

---
+++ Javascript/Lesson 24/index.js
@@ -0,0 +1,10 @@
+//learning about the array data type. multiple values stored in a single variable, comma delimited and inside brackets.
+const myNYCDestinations = [
+  "🗽 Manhattan - Washington Square Park",
+  "🏟️ The Bronx - Yankee Stadium",
+  "🎡 Brooklyn - Coney Island",
+  "✈️ Queens - Astoria Park",
+  "🌉 Staten Island - Historic Richmond Town"
+];
+
+console.log(myNYCDestinations);
