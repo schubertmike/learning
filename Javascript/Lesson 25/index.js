61f4f529045094ae7f01e5040b28968acc9cbcad
Michael Schubert <mschubert112@gmail.com>
2025-04-02 14:13:18 -0700

    Create index.js

---
+++ Javascript/Lesson 25/index.js
@@ -0,0 +1,12 @@
+//learning about indeces. array indeces start at 0 (for most programming languages)
+//we can change the value of an existing element in the array
+//we can assign a new value to an index that doesn't exist yet as well
+//array index example below
+
+let groceryList = ["🥛 Milk", "🥑 Avocado", "🥚 Eggs", "🍞 Bread"]; //establishing array variable
+
+groceryList[2] = "🧈 Butter"; //replaced eggs with butter
+groceryList[4] = "🧼 Laundry Soap"; //added laundry soap at the end
+
+console.log(groceryList); //print array
+
