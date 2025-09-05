0c9422f99ed800dce04a0606a283beca82fc6dc5
Michael Schubert <mschubert112@gmail.com>
2025-09-04 19:57:53 -0700

    Create initial.py

---
+++ Python/Lesson 4/initial.py
@@ -0,0 +1,7 @@
+#intro to comments (though already known), and my first initial
+#I like katsu curry
+print('mm     mm')
+print('m  m m  m')
+print('m   m   m')
+print('m       m')
+print('m       m')
