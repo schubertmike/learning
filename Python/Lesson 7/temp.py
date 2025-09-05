848ec392426df7339666a8c9694f6b1f30411023
Michael Schubert <mschubert112@gmail.com>
2025-09-04 20:10:28 -0700

    Create temp.py

---
+++ Python/Lesson 7/temp.py
@@ -0,0 +1,9 @@
+#now learning about operators
+#operators consist of +, -, *, /
+
+#convert degrees F to degrees C
+#ideally I would make brook_temp dynamic in the future through an api call to get the temp
+brook_temp = 71
+F_to_C = (brook_temp - 32) / 1.8
+
+print(F_to_C)
