d3dffc8305f0c9102af35f51135901ab28b78ec9
Michael Schubert <mschubert112@gmail.com>
2025-09-04 19:59:28 -0700

    Create letter.py

---
+++ Python/Lesson 5/letter.py
@@ -0,0 +1,7 @@
+#printing more stuff
+print('9/4/2025')
+print('I feel amazing')
+print('I want to code hardware and make robots. That will require python and C++ knowledge')
+print('woah you can make that?!')
+print('My favorite emoji is probably the laughing crying emoji')
+print('see! 😂')
