a4648017499c4ed480a7ccd2f98a3d7c8ccf68f9
Michael Schubert <mschubert112@gmail.com>
2025-09-04 20:04:48 -0700

    Create variables.py

---
+++ Python/Lesson 6/variables.py
@@ -0,0 +1,12 @@
+#intro to variables
+#there are some data types: integer, float, string, boolean
+integer = 5 #number
+string = "this is a string" #words, can use single or double quotes
+fl_oat = 3.14159 #decimal number
+boolean = True #true or false
+
+#print each of the variables from above
+print(integer)
+print(string)
+print(fl_oat)
+print(boolean)
