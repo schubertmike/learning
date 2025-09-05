bc85fae27c27b0249c39f95e94a2e1b8f3cc2ffd
Michael Schubert <mschubert112@gmail.com>
2025-09-04 20:14:50 -0700

    Create bmi.py

---
+++ Python/Lesson 8/bmi.py
@@ -0,0 +1,8 @@
+#intro to 2 new operators = modulo % and exponent **
+#modulo will return the remainder, exponents are self-explanatory
+weight_kg = 107.5
+height = 1.8288
+
+bmi = weight_kg / (height ** 2)
+
+print(bmi)
