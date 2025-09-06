2a8f936a36f3609f16c9a683dbffbf0955a2e2ac
Michael Schubert <mschubert112@gmail.com>
2025-09-05 21:47:57 -0700

    Create math.py

---
+++ Python/Lesson 9/math.py
@@ -0,0 +1,17 @@
+#learning about input and int
+#input captures a... input from the user
+#int can convert something to an integer
+
+#we can use these to capture the input, then do math with it. So here's the pythagorean theorem
+
+a = int(input("How long is the A side of the triangle?"))
+
+print(a)
+
+b = int(input("How long is the B side of the triangle"))
+
+print(b)
+
+c = ((a**2) + (b**2))**0.5
+
+print(c)
