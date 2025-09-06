5969ba84bf04cdb7473a20b17afe9c3a48aac971
Michael Schubert <mschubert112@gmail.com>
2025-09-06 11:21:18 -0700

    Create grading.py

---
+++ Python/Lesson 12/grading.py
@@ -0,0 +1,11 @@
+#learning about the if statement
+#it evals the statement so if condition a is met, then do something
+#we can add else to say otherwise, do something else
+
+#this variable is not dynamic right now
+grade = 56
+
+if grade > 55:
+  print('you passed!')
+else:
+  print('you failed :(')
