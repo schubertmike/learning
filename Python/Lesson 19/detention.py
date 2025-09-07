ec16b93f41bc6d78ee9282dbb445572f13633e2d
Michael Schubert <mschubert112@gmail.com>
2025-09-07 15:25:45 -0700

    Create detention.py

---
+++ Python/Lesson 19/detention.py
@@ -0,0 +1,5 @@
+#intro to the for loop and the range function
+#this will iterate the message 100 times 
+#the range starts at 0. For this instance, we can do range 100. For other's we may need to specify our range
+for i in range(101):
+  print('I will not use Snapchat in class')
