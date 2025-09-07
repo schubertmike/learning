8207fa970a2f7919c885f89a8553b1c332e6204e
Michael Schubert <mschubert112@gmail.com>
2025-09-06 20:54:53 -0700

    Create whille.py

---
+++ Python/Lesson 17/whille.py
@@ -0,0 +1,10 @@
+#introduction to loops, starting with while loop
+print('BANK OF CODÉDEX')
+
+pin = int(input('Enter your PIN: '))
+
+while pin != 1234:
+  pin = int(input('Incorrect PIN. Enter your PIN again: '))
+
+if pin == 1234:
+  print('PIN accepted!')
