67c86f145bb446209e13d5d0c153b01921c4b91e
Michael Schubert <mschubert112@gmail.com>
2025-09-07 16:54:21 -0700

    Create asterisk.py

---
+++ Python/Challenges/loops/asterisk.py
@@ -0,0 +1,4 @@
+#this is insane and I still don't quite know how it works
+#the part I don't understand is how the string multiplied by i will = copies of the string
+for i in range(1, 25):
+  print('* ' * i)
