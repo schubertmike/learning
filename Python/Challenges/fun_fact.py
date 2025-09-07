4b10eab4cc77e06a2283dc8c7bd480bab4f4c944
Michael Schubert <mschubert112@gmail.com>
2025-09-06 20:17:10 -0700

    Create fun_fact.py

---
+++ Python/Challenges/fun_fact.py
@@ -0,0 +1,17 @@
+#using random integer to tell fun facts
+import random
+
+fact = random.randint(0,5)
+
+if fact == 0:
+  print('Flamingos turn pink from eating shrimp.')
+elif fact == 1:
+  print('The only food that doesn\'t spoil is honey.')
+elif fact == 2:
+  print('Shrimp can only swim backwards.')
+elif fact == 3:
+  print('A taste bud\'s life span is about 10 days.')
+elif fact == 4:
+  print('It is impossible to sneeze while sleeping.')
+elif fact == 5:
+  print('It is illegal to sing off-key in North Carolina.')
