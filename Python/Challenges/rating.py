787592b315fa7e17c75dfcb1f6387253ce082479
Michael Schubert <mschubert112@gmail.com>
2025-09-06 20:09:55 -0700

    Create rating.py

---
+++ Python/Challenges/rating.py
@@ -0,0 +1,14 @@
+#creating a very simple rating system to print the verbal rating depending on the number value
+
+rating = 4.6
+
+if rating > 4.5:
+  print('Extraordinary')
+elif rating > 4:
+  print('Excellent')
+elif rating > 3:
+  print('Good')
+elif rating > 2:
+  print('Fair')
+else:
+  print('Poor')
