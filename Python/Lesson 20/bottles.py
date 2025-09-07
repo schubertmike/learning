f5633321a4ac499c3032b8b550263f28c8347c85
Michael Schubert <mschubert112@gmail.com>
2025-09-07 15:49:38 -0700

    Create bottles.py

---
+++ Python/Lesson 20/bottles.py
@@ -0,0 +1,11 @@
+#turning integers into strings through the str function and the f{} string interpolation
+#this is really a larger practice for range. reversing the sequence of the range allows us to go from x to 0 or lower
+for i in range(99, 0, -1):
+    print(str(i) + ' bottles of beer on the wall')
+    print('')
+    print(str(i) + ' bottles of beer')
+    print('')
+    print('take one down, pass it around')
+    print('')
+    print(str(i) + ' bottles of beer on the wall')
+    print('')
