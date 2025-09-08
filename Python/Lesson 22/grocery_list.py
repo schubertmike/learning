21cdb459021a9371e47c2b6f25a6f3f7f9f474e2
Michael Schubert <mschubert112@gmail.com>
2025-09-08 14:26:45 -0700

    Create grocery_list.py

---
+++ Python/Lesson 22/grocery_list.py
@@ -0,0 +1,7 @@
+#learning about lists
+#in order to keep track of things like multiple variables, we can use many variables, OR, better yet, use lists
+#we use square brackets to create the list and separate items with commas
+
+grocery = ['Eggs', 'Avocados', 'Cookies', 'Hot Pepper Jam', 'Blueberries', 'Broccoli']
+
+print(grocery)
