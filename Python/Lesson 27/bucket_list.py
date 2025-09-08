b2936f358d5410f9033a26cae07a3af782b93d05
Michael Schubert <mschubert112@gmail.com>
2025-09-08 16:06:12 -0700

    Create bucket_list.py

---
+++ Python/Lesson 27/bucket_list.py
@@ -0,0 +1,14 @@
+things_to_do = [
+  'Travel to Asia',
+  'open a non-profit that builds local pocket parks',
+  'learn the guitar',
+  'learn the piano',
+  'collect at least 1000 books',
+  'learn another language',
+  'climb a mountain',
+  'complete a triathlon',
+  'create an app that helps people find cheap groceries and restaurants'
+]
+
+for i in range(len(things_to_do)):
+  print(things_to_do[i])
