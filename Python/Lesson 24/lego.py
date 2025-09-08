99c52b005fc032c94a8ccb0007bc6cf9dd8b8b19
Michael Schubert <mschubert112@gmail.com>
2025-09-08 15:41:16 -0700

    Create lego.py

---
+++ Python/Lesson 24/lego.py
@@ -0,0 +1,7 @@
+"""now we're learning about built in functions.
+These include print(), str(), len(), max(), min(), and more (find some documentation for more)"""
+#now we are finding the minimum and maximum lego part numbers
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+
+print(min(lego_parts))
+print(max(lego_parts))
