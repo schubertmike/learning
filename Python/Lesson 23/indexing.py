7d0b54826628487fbf1f5186a45009e9c9529e28
Michael Schubert <mschubert112@gmail.com>
2025-09-08 15:37:51 -0700

    Create indexing.py

---
+++ Python/Lesson 23/indexing.py
@@ -0,0 +1,13 @@
+#creating a to-do list and learning about indexing
+#we can change items when we understand the index. The index starts at 0 and goes up from there
+#index can be negative too. So the last number in the list is -1 and it goes back from there
+#we can slice, which is accessing certain points by calling [start : end] to get those items
+
+todo = ['Get quarters','Do Laundry','Take a walk','Get a haircut','Make some tea','Complete Lists chapter','Call mom','Watch My Hero Academia']
+
+#prints first item
+print(todo[0])
+#prints 2nd item
+print(todo[1])
+#prints 3,4, and 5th item
+print(todo[2:5])
