7cf7dc79b9adfd819c52679b425b88c08ebcd864
Michael Schubert <mschubert112@gmail.com>
2025-09-09 15:45:36 -0700

    Update calculator.py

--- Python/Lesson 31/calculator.py
+++ Python/Lesson 31/calculator.py
@@ -1,1 +1,21 @@
+#learning return vs print
+'''use return inside a function when we want to send the values from one point to another in the code.
+use print when we want to show the output to the user'''
 
+def add(a,b):
+  return a + b
+def subtract(a,b):
+  return a - b
+def multiply(a,b):
+  return a * b
+def divide(a,b):
+  return a / b
+def exp(a,b):
+  return a ** b
+
+#all answers are equal to 6 lol
+print(add(3,3))
+print(subtract(9,3))
+print(multiply(3,2))
+print(divide(12,2))
+print(exp(6,1))
