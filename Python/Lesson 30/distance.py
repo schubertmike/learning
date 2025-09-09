09eab3f8ca73a5762c8a3cd1af9d69e310447752
Michael Schubert <mschubert112@gmail.com>
2025-09-09 15:28:31 -0700

    Create distance.py

---
+++ Python/Lesson 30/distance.py
@@ -0,0 +1,9 @@
+#now learning about function parameters
+#these are inputs that allow us to add some more dynamicism
+
+'''this is a little hard to wrap my head around. But, it sounds like, we can set the parameter name, 
+and then set the specific parameter after the fact, and it will replace the parameter name with that argument'''
+
+def distance_to_miles(distance):
+  print(distance / 1.609)
+distance_to_miles(10000)
