784f17e294757fa0443f72398230316d26d63a50
Michael Schubert <mschubert112@gmail.com>
2025-09-11 16:47:55 -0700

    Update average.py

--- Python/Challenges/functions/average.py
+++ Python/Challenges/functions/average.py
@@ -5,7 +5,7 @@ def average(num1, num2):
 print(average(5,10))
 
 #you can also do it like this, and remove the step of setting the local variable
-def avg(num1, num2):
+'''def avg(num1, num2):
   return (num1 + num2)/2
 
-print(avg(5,10))
+print(avg(5,10))'''
