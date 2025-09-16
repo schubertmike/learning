c7c97041bc9271f711b8fd0e9d31ba10afac775a
Michael Schubert <mschubert112@gmail.com>
2025-09-16 16:46:36 -0700

    Create bank.py

---
+++ Python/Lesson 37/bank.py
@@ -0,0 +1,3 @@
+'''we can create functions within classes. We can create our own methods within the classes too
+then we call the method and run it
+the first argument within the method inside the class is self'''
