adbac3e25c1dd9a7da295894a94cb653f41555a9
Michael Schubert <mschubert112@gmail.com>
2025-09-16 16:07:59 -0700

    Create class.py

---
+++ Python/Lesson 34/class.py
@@ -0,0 +1,7 @@
+#introduction to classes in python. They allow us to templatize objects created using the class
+#it's the blueprint for the object
+class Restaurant:
+  name = ''
+  category = ''
+  rating = 0.0
+  delivery = False
